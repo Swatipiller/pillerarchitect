c0d2b5874e0039a99d83ce353415a19600931036
Swatipiller <128450338+Swatipiller@users.noreply.github.com>
2023-06-26 22:42:50 +0530

    Add files via upload

--- phpmailer-contact-form/phpmailer-contact-form/handler.php
+++ phpmailer-contact-form/phpmailer-contact-form/handler.php
@@ -21,6 +21,6 @@ $validator->field('Message')->maxLength(6000);
 
 
 
-$pp->sendEmailTo('kolatepatil6789@gmail.com'); // ← Your email here
+$pp->sendEmailTo('umesh.kolate@unbxd.com'); // ← Your email here
 
 echo $pp->process($_POST);
